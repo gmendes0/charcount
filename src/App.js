6e2e2e92e188005dc2f6e101a74cf6cd8b62ea89
mendesg0 <gabriel@prestige.com.br>
2020-05-29 10:35:47 -0300

    feat: initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+import "./style.css";
 
 function App() {
+  const [text, setText] = useState('');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Character Couter</h1>
+      <p className='text'>Your text have <span className='characteres'>{text.length}</span> characters.</p>
+      <textarea placeholder='Type here' onChange={event => setText(event.target.value)}>{text}</textarea>
     </div>
   );
 }
