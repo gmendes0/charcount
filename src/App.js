61786853e86b7883c6d08fa6715183f713b69e59
mendesg0 <gabriel@prestige.com.br>
2020-05-29 11:08:32 -0300

    fix: remove \n from count

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <h1>Character Couter</h1>
-      <p className='text'>Your text have <span className='characteres'>{text.length}</span> characters.</p>
+      <p className='text'>Your text have <span className='characteres'>{text.replace(/(\r\n|\n|\r)/g, '').length}</span> characters.</p>
       <textarea placeholder='Type here' onChange={event => setText(event.target.value)}>{text}</textarea>
     </div>
   );
